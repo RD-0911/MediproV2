86eb5882c61ba69966ca4f52a77697f84aafd1a9
Amanda Gutierman <agutierman@gmail.com>
2025-10-06 20:18:08 -0600

    AMANDA: Creación de mi rama

--- app/login.tsx
+++ app/login.tsx
@@ -3,10 +3,10 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator,
 import { Link, useRouter } from 'expo-router';
 
 // Usa la IP LAN que te salió en ipconfig
-const API_URL = 'http://192.168.100.55:3000';
-console.log('[API_URL]', API_URL);
+//const API_URL = 'http://192.168.100.55:3000';
+//console.log('[API_URL]', API_URL);
 
-//const API_URL = 'http://localhost:3000'; // cambia por tu IP si usas Expo Go en celular
+const API_URL = 'http://localhost:3000'; // cambia por tu IP si usas Expo Go en celular
 
 
 export default function LoginScreen() {
